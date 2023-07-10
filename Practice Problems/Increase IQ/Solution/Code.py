8e8e0e8e0415f2fbcb94596c3c71f0e1d32eb6da
Riddhi's Chaotic Universe <130882317+Riddhiman2005@users.noreply.github.com>
2023-07-11 01:11:44 +0530

    IQ

---
+++ Practice Problems/Increase IQ/Solution/Code.py
@@ -0,0 +1,12 @@
+
+# Read the current IQ of Chef
+x = int(input())
+
+# Calculate the IQ after learning to play a musical instrument
+iq_after_learning = x + 7
+
+# Check if Chef's IQ will become greater than Einstein's IQ
+if iq_after_learning > 170:
+    print("Yes")
+else:
+    print("No")
