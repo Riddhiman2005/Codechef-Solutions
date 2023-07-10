059c828996f900e3a67ae92ac60d1c10b0a1bbf3
Riddhi's Chaotic Universe <130882317+Riddhiman2005@users.noreply.github.com>
2023-07-10 14:02:04 +0530

    Squats

---
+++ Practice Problems/Squats/Solutions/Code.py
@@ -0,0 +1,13 @@
+# Read the number of test cases
+T = int(input())
+
+# Iterate over the test cases
+for _ in range(T):
+    # Read the number of sets
+    X = int(input())
+
+    # Calculate the total number of squats
+    total_squats = X * 15
+
+    # Print the result
+    print(total_squats)
