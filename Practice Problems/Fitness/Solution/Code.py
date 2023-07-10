149366898ec8a9eda4ab39637b03d265f1d5ac7e
Riddhi's Chaotic Universe <130882317+Riddhiman2005@users.noreply.github.com>
2023-07-10 14:41:53 +0530

    Fitness Code

---
+++ Practice Problems/Fitness/Solution/Code.py
@@ -0,0 +1,14 @@
+
+# Read the number of test cases
+T = int(input())
+
+# Iterate over the test cases
+for _ in range(T):
+    # Read the distance to the office
+    X = int(input())
+
+    # Calculate the number of kilometers Chef travels in a week
+    weekly_distance = X * 2 * 5
+
+    # Print the result
+    print(weekly_distance)
