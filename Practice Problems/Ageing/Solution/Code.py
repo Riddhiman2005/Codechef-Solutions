ca9a9e4afee18908183e66541b4230d5dbaa239b
Riddhi's Chaotic Universe <130882317+Riddhiman2005@users.noreply.github.com>
2023-07-10 20:36:35 +0530

    Ageing

---
+++ Practice Problems/Ageing/Solution/Code.py
@@ -0,0 +1,8 @@
+
+T = int(input())  # Read the number of test cases
+
+for _ in range(T):
+    X = int(input())  # Read the age of Chef
+
+    Chefina_age = X - 10  # Calculate Chefina's age when Chef will be X years old
+    print(Chefina_age)  # Print Chefina's age
