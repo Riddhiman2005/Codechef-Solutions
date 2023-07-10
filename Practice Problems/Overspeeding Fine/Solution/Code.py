b65f45cfe5d6f14d7fd617f3df34521a83bbf830
Riddhi's Chaotic Universe <130882317+Riddhiman2005@users.noreply.github.com>
2023-07-10 19:44:03 +0530

    Fine for Overspeeding

--- Practice Problems/Overspeeding Fine/Solution/Code.py
+++ Practice Problems/Overspeeding Fine/Solution/Code.py
@@ -1,12 +1,11 @@
-
 T = int(input())  # Read the number of test cases
 
 for _ in range(T):
     X = int(input())  # Read the speed of Chef's car
 
     if X <= 70:
-        print("No Fine")  # No fine if the speed is less than or equal to 70
+        print("0")  # No fine if the speed is less than or equal to 70
     elif X > 70 and X <= 100:
-        print("Rs 500 Fine")  # Rs 500 fine if the speed is greater than 70 and less than or equal to 100
+        print("500")  # Rs 500 fine if the speed is greater than 70 and less than or equal to 100
     else:
-        print("Rs 2000 Fine")  # Rs 2000 fine if the speed is greater than 100
+        print("2000")  # Rs 2000 fine if the speed is greater than 100
