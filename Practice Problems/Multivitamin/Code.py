4e2fa82c4d09ff485a93c69e9491f4a60511519f
Riddhi's Chaotic Universe <130882317+Riddhiman2005@users.noreply.github.com>
2023-07-11 23:46:53 +0530

    Vitamans an Imp requirement

---
+++ Practice Problems/Multivitamin/Code.py
@@ -0,0 +1,13 @@
+# Read the number of test cases
+T = int(input())
+
+# Iterate over the test cases
+for _ in range(T):
+    # Read the input for each test case
+    X, Y = map(int, input().split())
+
+    # Check if Chef has enough tablets for the given days
+    if Y >= X * 3:
+        print("YES")
+    else:
+        print("NO")
