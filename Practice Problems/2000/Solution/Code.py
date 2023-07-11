603b90826e5660b01fd93af41be65838116af405
Riddhi's Chaotic Universe <130882317+Riddhiman2005@users.noreply.github.com>
2023-07-11 16:54:08 +0530

    No of 500 Notes needed

---
+++ Practice Problems/2000/Solution/Code.py
@@ -0,0 +1,12 @@
+
+# Read the number of Rs. 2000 notes, N
+N = int(input())
+
+# Calculate the total value of the Rs. 2000 notes
+total_value = N * 2000
+
+# Calculate the number of Rs. 500 notes needed
+notes_500 = total_value // 500
+
+# Print the number of Rs. 500 notes needed
+print(notes_500)
