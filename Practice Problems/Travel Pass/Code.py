33223c03958015aed39d428e06ee56291cbb242c
Riddhi's Chaotic Universe <130882317+Riddhiman2005@users.noreply.github.com>
2023-07-18 09:17:21 +0530

    From Fill Up

---
+++ Practice Problems/Travel Pass/Code.py
@@ -0,0 +1,24 @@
+# Function to calculate the total time Chef has to spend on filling the various forms
+def calculate_total_time(N, A, B, S):
+    total_time = 0
+    for i in range(N):
+        if S[i] == '0':
+            total_time += A
+        else:
+            total_time += B
+    return total_time
+
+# Read the number of test cases
+T = int(input())
+
+# Process each test case
+for _ in range(T):
+    # Read the input for each test case
+    N, A, B = map(int, input().split())
+    S = input()
+
+    # Calculate the total time
+    total_time = calculate_total_time(N, A, B, S)
+
+    # Print the result
+    print(total_time)
