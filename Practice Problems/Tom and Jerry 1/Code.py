10c3df1aa4f73ad174cb240161781db9bf9279f2
Riddhi's Chaotic Universe <130882317+Riddhiman2005@users.noreply.github.com>
2023-08-03 21:00:54 +0530

    TANDJ1

---
+++ Practice Problems/Tom and Jerry 1/Code.py
@@ -0,0 +1,14 @@
+
+t = int(input())
+
+for i in range(t):
+    a, b, c, d, K = map(int, input().split())
+    path = abs(c - a) + abs(b - d)
+    if K == path:
+        print("Yes")
+    if path > K:
+        print("No")
+    if (K - path) % 2 == 1 and K > path:
+        print("No")
+    if (K - path) % 2 == 0 and K > path:
+        print("Yes")
