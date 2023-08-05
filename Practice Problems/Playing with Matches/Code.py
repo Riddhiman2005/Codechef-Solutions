97a12528e7de9d57b2dd351bfc3ec995fb5abe91
Riddhi's Chaotic Universe <130882317+Riddhiman2005@users.noreply.github.com>
2023-08-05 16:31:59 +0530

    Playstr

---
+++ Practice Problems/Playing with Matches/Code.py
@@ -0,0 +1,15 @@
+t = int(input()) 
+
+M = [6, 2, 5, 5, 4, 5, 6, 3, 7, 6]  # List containing the number of matches needed for digits 0 to 9
+
+while t:
+    A, B = map(int, input().split())  
+    m = 0  
+    S = str(A + B)  # Calculate the sum of A and B, and convert it to a string
+    for i in S:
+        i = int(i)  # Convert the character to an integer
+        m = m + M[i]  # Add the number of matches needed for this digit
+        
+    print(m)  # Print the total number of matches needed
+
+    t = t - 1  
