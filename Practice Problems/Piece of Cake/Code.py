5ec4cffb32a9443b49c75ec69379c167f1e9e60c
Riddhi's Chaotic Universe <130882317+Riddhiman2005@users.noreply.github.com>
2023-08-06 22:19:36 +0530

    Piece of Cake

--- Practice Problems/Piece of Cake/Code.py
+++ Practice Problems/Piece of Cake/Code.py
@@ -1,23 +1,13 @@
-t = int(input())
+t = int(input())  
 
-for _ in range(t):
-    s = input()  
-    
-    # Creating a dictionary to store the frequency of each character in the string
-    char_count = {}
-    for char in s:
-        if char in char_count:
-            char_count[char] += 1
-        else:
-            char_count[char] = 1
-    
-    # Calculate the sum of frequencies of characters other than the current one
-  
-    other_sum = sum(value for key, value in char_count.items() if key != s[0])
-    
-    # Check if the frequency of the current character is equal to the sum of others
-  
-    if char_count[s[0]] == other_sum:
-        print("YES")
+while t:
+    n = str(input())  
+    for i in list(n):  
+        # Check if the count of the current character equals the sum of counts of other characters
+        if n.count(i) == (len(n) - n.count(i)):
+            print("YES")  # Print "YES" if the condition is satisfied
+            break  # Exit the loop since the condition is met
     else:
-        print("NO")
+        print("NO")  # Print "NO" if the loop completes without finding a character that satisfies the condition
+    
+    t = t - 1  
