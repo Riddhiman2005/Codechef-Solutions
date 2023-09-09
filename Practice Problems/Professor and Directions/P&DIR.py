ce9c15f4fc3609efc4208b1a3e19ff5ebe13311a
Riddhi's Chaotic Universe <130882317+Riddhiman2005@users.noreply.github.com>
2023-09-09 20:13:42 +0530

    P&DIR

---
+++ Practice Problems/Professor and Directions/P&DIR.py
@@ -0,0 +1,12 @@
+# cook your dish here
+for _ in range(int(input())):
+    t= input()
+    
+    t= input()
+    
+    if "RR" in t: 
+        print("YES")
+    elif "LL" in t:
+        print("YES")
+    else:
+        print("NO")
